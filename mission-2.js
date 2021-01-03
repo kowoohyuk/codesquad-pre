d8fe4d6fdea3c00164a9df1c50d06238ff86285a
kowoohyuk <kowoohyuk91@gmail.com>
2021-01-03 18:20:31 +0900

    두 번째 미션, mission-2.js

---
+++ mission-2.js
@@ -0,0 +1,98 @@
+const getKeyName = arr => {
+  const result = [];
+  arr.forEach(v => {
+    if(v.type === 'sk') {
+      result.push(v.name);
+    }
+    if(v.childnode.length > 0) {
+      result.push(...getKeyName(v.childnode));
+    }
+  });
+  return result;
+};
+
+const test = () => {
+  const arr = [{
+    "id": 1,
+    "name": "Yong",
+    "phone": "010-0000-0000",
+    "type": "sk",
+    "childnode": [{
+      "id": 11,
+      "name": "echo",
+      "phone": "010-0000-1111",
+      "type": "kt",
+      "childnode": [{
+          "id": 115,
+          "name": "hary",
+          "phone": "211-1111-0000",
+          "type": "sk",
+          "childnode": [{
+            "id": 1159,
+            "name": "pobi",
+            "phone": "010-444-000",
+            "type": "kt",
+            "childnode": [{
+                "id": 11592,
+                "name": "cherry",
+                "phone": "111-222-0000",
+                "type": "lg",
+                "childnode": []
+              },
+              {
+                "id": 11595,
+                "name": "solvin",
+                "phone": "010-000-3333",
+                "type": "sk",
+                "childnode": []
+              }
+            ]
+          }]
+        },
+        {
+          "id": 116,
+          "name": "kim",
+          "phone": "444-111-0200",
+          "type": "kt",
+          "childnode": [{
+            "id": 1168,
+            "name": "hani",
+            "phone": "010-222-0000",
+            "type": "sk",
+            "childnode": [{
+              "id": 11689,
+              "name": "ho",
+              "phone": "010-000-0000",
+              "type": "kt",
+              "childnode": [{
+                  "id": 116890,
+                  "name": "wonsuk",
+                  "phone": "010-000-0000",
+                  "type": "kt",
+                  "childnode": []
+                },
+                {
+                  "id": 1168901,
+                  "name": "chulsu",
+                  "phone": "010-0000-0000",
+                  "type": "sk",
+                  "childnode": []
+                }
+              ]
+            }]
+          }]
+        },
+        {
+          "id": 117,
+          "name": "hong",
+          "phone": "010-0000-0000",
+          "type": "lg",
+          "childnode": []
+        }
+      ]
+    }]
+  }]
+  console.log(getKeyName(arr));
+}
+
+test();
